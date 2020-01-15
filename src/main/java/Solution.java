4aca5ea327d1e0aef9019ba3c37855df1778e5c9
yangmeng <yangmeng@sdiread.com>
2020-01-15 10:55:26 +0800

    1

---
+++ src/main/java/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+
+	public int rectangleArea(int[][] rectangles) {
+		for (int[] is : rectangles) {
+		}
+
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		int[][] rectangles = new int[][]{{0, 0, 2, 2}, {1, 0, 2, 3}, {1, 0, 3, 1}};
+	}
+}
